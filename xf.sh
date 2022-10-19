33eda83385459da01a9f2dac3c4fbe5b2f6fdc5d
esome-zz <stu-gyx@qq.com>
2022-10-19 16:53:03 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -41,9 +41,7 @@ fi
 
 # Prepare
 echo "Prepare to use"
-unzip v2ray.zip && chmod +x v2ray v2ctl
-mv v2ray v2ctl /usr/bin/
-mv geosite.dat geoip.dat /usr/local/share/v2ray/
+
 # mv config.json /etc/v2ray/config.json
 
 # Set config file
